d7e775f827f187c015f7f71b5eb90ab0f3673ae0
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-12 14:42:34 +0100

    creating fake records

---
+++ database/factories/TrainFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class TrainFactory extends Factory
+{
+    
+    public function definition(): array
+    {
+        return [
+            'company' => fake()->company(),
+            'departure_station' => fake()->city() . 'Station',
+            'arrival_station' => fake()->city() . 'Station',
+            'departure_time' => fake()->time(),
+            'arrival_time' => fake()->time(),
+            'train_code' => fake()->randomNumber(4, true),
+            'carriages_number' => fake()->randomDigitNotNull(),
+            'is_delayed' => fake()->boolean(),
+            'is_cancelled' => fake()->boolean()
+        ];
+    }
+}
--- database/migrations/2024_03_12_130850_create_trains_table.php
+++ database/migrations/2024_03_12_130850_create_trains_table.php
@@ -14,12 +14,12 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
 
             $table->id();
-            
+
             $table->string('company', 50);
             $table->string('departure_station', 100);
             $table->string('arrival_station', 100);
-            $table->time('departure_time', 4);
-            $table->time('arrival_time', 4);
+            $table->time('departure_time');
+            $table->time('arrival_time');
             $table->smallInteger('train_code')->unsigned()->unique();
             $table->tinyInteger('carriages_number')->unsigned();
             $table->boolean('is_delayed')->default(0);
