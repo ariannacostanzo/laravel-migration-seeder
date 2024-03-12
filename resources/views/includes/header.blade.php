be65e1502f825d963378536f34b72eded2593e2d
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-12 14:04:56 +0100

    creating templates

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
-</div>
+<nav class="navbar bg-dark border-bottom border-body navbar-expand-lg" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link @if (Route::is('home')) active @endif" aria-current="page" href="{{route('home')}}">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Treni</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Prezzi</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -4,19 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{env(APP_NAME)}} | @yield('title')</title>
+    <title>{{env('APP_NAME')}} | @yield('title')</title>
     @vite('resources/js/app.js')
     @yield('cdns')
 </head>
 <body>
 
     {{-- header --}}
-
+    @include('includes.header')
     {{-- main --}}
     <div class="container">
         @yield('main-content')
     </div>
 
     {{-- footer --}}
+
+    @yield('scripts')
 </body>
 </html>
