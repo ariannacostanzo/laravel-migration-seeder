c2a5788905e3c12570e33a5f2d245a8e2ee46854
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-12 13:48:59 +0100

    first commit

---
+++ resources/views/templates/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{env(APP_NAME)}} | @yield('title')</title>
+    @vite('resources/js/app.js')
+    @yield('cdns')
+</head>
+<body>
+
+    {{-- header --}}
+
+    {{-- main --}}
+    <div class="container">
+        @yield('main-content')
+    </div>
+
+    {{-- footer --}}
+</body>
+</html>
